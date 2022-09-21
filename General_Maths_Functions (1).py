28c178895db54214ad19f99037d3d74236b94ffc
Araf-Ezaz <arafezazgaminglegend@gmail.com>
2022-09-21 10:13:38 +1000

    س

---
+++ General_Maths_Functions (1).py
@@ -0,0 +1,27 @@
+def numbercheck(number):
+    try:
+        float(number)
+    except:
+        return False
+    else:
+        return True
+
+def intcheck(number):
+    try:
+        float(number)
+    except:
+        return False
+    else:
+        return float(number).is_integer()
+
+def intinator(number):
+    if type(number) == float or type(number) == int:
+        if float(number).is_integer():
+            return int(number)
+        else:
+            return number
+    else:
+        raise ValueError("Requires a 'float' or 'int' object")
+
+def ezinput(msg):
+    return input(msg).strip().lower()
