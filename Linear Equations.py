ba9fd87117ec9f5a42a5fa21f5e59b124855819c
Goodman <arafezazgaminglegend@gmail.com>
2022-10-20 15:09:34 +1100

    h

--- Linear Equations.py
+++ Linear Equations.py
@@ -1,15 +1,15 @@
 from General_Maths_Functions import *
-
+from fractions import Fraction
 class coord():
     def __init__(self,tuple):
         self.x = tuple[0]
         self.y = tuple[1]
 
 def coinput(txt):
-    txt = input(txt)
-    txt = tuple(map(float,txt.split(',')))
+    txt = tuple(map(float,input(txt).split(',')))
     if len(txt) != 2:
         raise ValueError
+    return txt
 
 if __name__ == "__main__":
     while True:
@@ -19,3 +19,20 @@ if __name__ == "__main__":
         except:
             print("Invalid Input")
             continue
+        mid = lambda a, b: intinator((a+b)/2)
+        print(f"\nMidpoint of the line between the points is ({mid(c1.x,c2.x)},{mid(c1.y,c2.y)})")
+        x = c2.x - c1.x
+        y = c2.y - c1.y
+        length = intinator(x**2+y**2)
+        length = int(length**(1/2)) if intcheck(length**(1/2)) else f"√{length}"
+        print(f"pain {length}")
+        if y == 0:
+            gradient = "Undefined"
+        elif x == 0:
+            gradient = 0
+        else:
+            if x < 0:
+                x *= -1
+                y *= -1
+            gradient = Fraction(intinator(y),intinator(x))
+        print("The gradient of the line between the points is",gradient)
