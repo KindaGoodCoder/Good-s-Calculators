9b5c640cba969dc17e6c3dbfd1b36dbd53295851
Goodman <arafezazgaminglegend@gmail.com>
2023-03-29 08:08:26 +1100

    Create Sequences.cpp

---
+++ Sequences.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+//Sequence Calculator
+
+int main() {
+    std::string mode;
+    std::cout << "Arthmetic or geometric? ";
+
+    std::cin >> mode;
+    std::cout << mode << std::endl;
+    return 0
+}
