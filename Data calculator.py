a349892177c00f28f9132f403df4607b6bdb8e76
Goodman <arafezazgaminglegend@gmail.com>
2023-04-02 13:36:06 +1000

    Standard Deviation

--- Data calculator.py
+++ Data calculator.py
@@ -1,4 +1,5 @@
-from General_Maths_Functions import intinator #Import from another file
+from General_Maths_Functions import intinator,math #Import from another file
+#Also import math module from General_Math_Functions cause y not
 
 print("Example input: 26,27,29") #Example input
 
@@ -16,21 +17,31 @@ if __name__ == "__main__":
         print("\nNumber of data provided: ", data_length)
         print("Sum of all data provided: ", data_sum)
 
-        print(f"The Mean is: {intinator(round((data_sum/data_length),2))}")
+        mean = data_sum/data_length
+        print(f"The Mean is: {intinator(mean)}")
 
+        #kdata is placeholder value, just holds values for each operation
         kdata = max(data)-min(data) #kdata = range
         kdata = 1 if kdata == 0 else kdata        
-        print(f"The Range is: {kdata}")
+        print(f"The Range is:",kdata)
 
+        #amode is same as kdata's role
         amode = int(data_length/2) #amode = median
-        print("The Median is: ", intinator((data[amode]+data[amode-1])/2) if data_length % 2 == 0 else data[amode])
+        print("The Median is:", intinator((data[amode]+data[amode-1])/2) if data_length % 2 == 0 else data[amode])
 
-        #IQR      
-        kdata = int((amode-1)/2)
-        q1 = intinator((data[kdata]+data[kdata+1])/2) if data_length % 4 != 0 else data[kdata]
-        kdata = int((data_length+kdata+1)/2)
-        q2 = intinator((data[kdata]+data[kdata+1])/2) if data_length % 4 != 0 else data[kdata]
-        print(q1,q2)
+        #Standard Deviation
+        kdata = 0
+        for x in data: kdata += (x-mean)**2 #For each value, find the square of the x-mean
+        print("Standard Deviation is",intinator(math.sqrt(kdata/data_length)))
+
+        #IQR
+        if data_length > 3:
+            kdata = int((amode-1)/2)
+            q1 = intinator((data[kdata]+data[kdata+1])/2) if data_length % 4 != 0 else data[kdata]
+            kdata = int((data_length+kdata+1)/2)
+            #Q2 aint working. Too lazy to fix
+            q2 = intinator((data[kdata]+data[kdata+1])/2) if data_length % 4 != 0 else data[kdata]
+            print("Q1 is:",q1)
         
         # print("IQR is: ",kdata)
 
--- General_Maths_Functions.py
+++ General_Maths_Functions.py
@@ -1,3 +1,4 @@
+import math
 def numbercheck(number):
     try:
         float(number)
